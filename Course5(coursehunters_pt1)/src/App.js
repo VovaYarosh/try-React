92e79894c63e8f9a2400fd5d8b84509583ee9b11
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-01 21:20:20 +0200

    edited

---
+++ Course5(coursehunters_pt1)/src/App.js
@@ -0,0 +1,119 @@
+import React,{Component} from 'react';
+
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import './App.scss';
+import Car from './Car/Car'
+import Counter from './Counter/Counter'
+
+
+
+export const ClickedContext = React.createContext(false);
+
+class App extends Component {
+
+    constructor(props) {
+        console.log('app constructor');
+        super(props);
+
+        this.state = {
+            clicked: false,
+            cars: [
+                {name: 'Ford', year: 2010},
+                {name: 'Mazda', year: 2007},
+                {name: 'Volvo', year: 2001}
+            ],
+            pageTitle: 'React components',
+            showCars: false
+        }
+    }
+
+    onChangeName(name,index){
+        const car = this.state.cars[index];
+        car.name = name;
+        const cars = [...this.state.cars];
+        cars[index] = car;
+        this.setState({
+            cars
+        })
+    }
+
+    deleteHandler = (index) =>{
+        const cars = this.state.cars.concat();
+        cars.splice(index,1);
+        this.setState(
+            {cars}
+        )
+    };
+
+    toggleCarsHandler = () =>{
+        this.setState({
+           showCars: !this.state.showCars
+        });
+    };
+
+    componentWillMount(){
+        console.log('App componentWillMount')
+    }
+    componentDidMount(){
+        console.log('App componentDidMount')
+    }
+
+
+  render(){
+        console.log('App render');
+    const divStyle = {
+      'textAlign' : 'center',
+    };
+
+    let cars = null;
+
+    if(this.state.showCars){
+        cars = this.state.cars.map((car,index)=>{
+            return (
+                <ErrorBoundary key={index}>
+                <Car
+                    name={car.name}
+                    year={car.year}
+                    index={index}
+                    onChangeName={ event =>
+                    this.onChangeName(event.target.value,index)}
+                    onDelete={this.deleteHandler.bind(this, index)}
+                />
+                </ErrorBoundary>
+            )
+        })
+    }
+
+
+    return(
+        <div style={divStyle}>
+          {/*<h1>{this.state.pageTitle}</h1>*/}
+          <h1>{this.props.title}</h1>
+
+        <ClickedContext.Provider value={this.state.clicked}>
+            <Counter />
+        </ClickedContext.Provider>
+
+
+            <hr/>
+          <button
+              style={{marginTop:20}}
+              className={'AppButton'}
+              onClick={this.toggleCarsHandler}>
+              Toggle cars</button>
+
+          <button onClick={()=>this.setState({clicked: true})}>Change clicked</button>
+
+          <div style={{
+               width: 400,
+               margin: 'auto',
+               paddingTop: '20px'
+            }}>
+            {cars}
+            </div>
+        </div>
+    )
+  }
+}
+
+export default App;
---
+++ Course5(coursehunters_pt1)/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App title={'I am from props!'}/>, document.getElementById('root'));
+
+serviceWorker.unregister();
