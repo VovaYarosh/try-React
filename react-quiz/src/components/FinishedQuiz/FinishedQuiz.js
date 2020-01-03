f4d66bc1ada1eab1ee9abf9a20f4d961ce22e290
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-03 23:19:37 +0200

    add validation

--- react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
+++ react-quiz/src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.css'
 import Button from '../UI/Button/Button'
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz = props =>{
     const successCount = Object.keys(props.results).reduce((total,key)=>{
@@ -47,7 +48,9 @@ const FinishedQuiz = props =>{
 
             <div>
                 <Button onClick={props.onRetry} type='primary'>retry</Button>
+                <Link to="/">
                 <Button type='success'>go to quiz-list</Button>
+                </Link>
             </div>
         </div>
     )
