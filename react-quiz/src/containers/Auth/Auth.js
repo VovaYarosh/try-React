b25c5f9de697eb0dfc068f93dd2e35ded30418bb
VovaYarosh <vovayaroshlenovo94@gmail.com>
2020-01-04 17:53:39 +0200

    validation add disabled

--- react-quiz/src/containers/Auth/Auth.js
+++ react-quiz/src/containers/Auth/Auth.js
@@ -8,6 +8,7 @@ import is from 'is_js'
 export default class Auth extends Component{
 
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                value: '',
@@ -81,8 +82,14 @@ export default class Auth extends Component{
 
         formControls[controlName] = control
 
+        let isFormValid = true
+
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
         this.setState({
-            formControls
+            formControls, isFormValid
         })
     }
 
@@ -118,6 +125,7 @@ export default class Auth extends Component{
                         <Button
                             type="success"
                             onClick={this.loginHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Enter
                         </Button>
@@ -125,6 +133,7 @@ export default class Auth extends Component{
                         <Button
                             type="primary"
                             onClick={this.registerHandler}
+                            disabled={!this.state.isFormValid}
                         >
                             Registration
                         </Button>
